9067323fa7dbfa5c44e804993f9cd86998f875f7
laurel Bonal <laurelbonal@gmail.com>
2024-01-31 16:04:56 -0700

    Coded challenges one and two

--- src/book.js
+++ src/book.js
@@ -1,11 +1,26 @@
-function createTitle() {
-  
+function createTitle(title) {
+  var bookIdea = `The ${title}`
+  return bookIdea
 }
+
+function buildMainCharacter(name, age, pronouns) { 
+  var character = {
+    name: name,
+    age: age, 
+    pronouns: pronouns
+  } 
+  return character
+}
+
+function saveReview(criticReviews, allReviews) {
+  allReviews.push(criticReviews)
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
