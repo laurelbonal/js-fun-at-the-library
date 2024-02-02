683b789edad3f40cb533958ff670478d117862a4
laurel Bonal <laurelbonal@gmail.com>
2024-02-02 08:43:37 -0700

    Finsihed third problem, working on last problem

--- src/shelf.js
+++ src/shelf.js
@@ -3,7 +3,7 @@ module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  // searchShelf
+  searchShelf
 };
 
 function shelfBook(bookTitle, shelf){
@@ -22,5 +22,21 @@ function unshelfBook(bookTitle, shelf){
 }
 
 function listTitles(shelf){
-  return shelf.titles
+  var list = []
+  for (i = 0; i <shelf.length; i++){
+    list.push(shelf[i].title)
+  }
+  var titles = `${list[0]}, ${list[1]}, ${list[2]}`
+  return titles 
+} 
+
+
+function searchShelf(shelf, bookTitle){
+      for (i = 0; i < shelf.length; i++){
+        if (shelf[i].title === bookTitle)
+        return true 
+      else {
+        return false
+      }
+      }
 }
