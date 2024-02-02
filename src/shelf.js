10b2e6626bf5de739390fcf61fad6e00d20b2779
laurel Bonal <laurelbonal@gmail.com>
2024-02-01 20:28:44 -0700

    Worked through first two problems

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,26 @@
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
+
+function shelfBook(bookTitle, shelf){
+  if (shelf.length < 3){
+  shelf.unshift(bookTitle)}
+  return shelf
+}
+
+function unshelfBook(bookTitle, shelf){
+  for (i = 0; i < shelf.length; i++){
+    if (shelf[i].title === bookTitle){
+    shelf.splice(i, 1)
+    }
+   }
+   return shelf
+}
+
+function listTitles(shelf){
+  return shelf.titles
+}
