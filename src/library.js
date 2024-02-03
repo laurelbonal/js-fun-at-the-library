469451dd3d380edf47d30a8e22b309615cd4d488
laurel Bonal <laurelbonal@gmail.com>
2024-02-02 19:34:44 -0700

    Completed first two functions

--- src/library.js
+++ src/library.js
@@ -1,8 +1,37 @@
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
+  createLibrary,
+  addBook,
+  checkoutBook,
   // takeStock
 };
+
+function createLibrary(libraryName){
+  library = {
+  name: libraryName,
+  shelves: {
+    fantasy: [],
+    fiction: [],
+    nonFiction: []
+  }
+  }
+  return library
+}
+
+
+function addBook(libraryName, bookName){
+     libraryName.shelves[bookName.genre].push(bookName) 
+
+    // for (i = 0; i < libraryName.shelves.length; i++){
+    //   if (libraryName.shelves.includes(bookName.genre)){
+    //     libraryName.shelves[i].push(bookName)
+    //   }
+    // }
+    // return libraryName.shelves
+    // console.log("sanity check", libraryName.shelves[0])
+}
+
+function checkoutBook(){
+  
+}
