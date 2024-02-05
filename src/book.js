9d2db3af72f701fc1125030bce4c28c2c856581b
laurel Bonal <laurelbonal@gmail.com>
2024-02-05 16:17:27 -0700

    Recommit last functions

--- src/book.js
+++ src/book.js
@@ -12,20 +12,41 @@ function buildMainCharacter(name, age, pronouns) {
   return character
 }
 
-function saveReview(criticReviews, allReviews) {
-  allReviews = []
-  allReviews.push(criticReviews)
- return allReviews
 
+function saveReview(criticReviews, allReviews) {
+    if(!allReviews.includes(criticReviews)){
+      allReviews.push(criticReviews)
+    }
+    return allReviews
 }
  
+function calculatePageCount(bookTitle) {
+    var bookPageCount = bookTitle.length * 20
+    return bookPageCount
+}
 
+function writeBook(bookTitle, character, genre){
+  var book = {
+    title: bookTitle,
+    mainCharacter: character,
+    pageCount: bookTitle.length * 20,
+    genre: genre
+  }
+  return book
+}
+
+
+function editBook(bookObject){
+  var newPageCount = bookObject.pageCount * .75
+  bookObject.pageCount = newPageCount
+  return bookObject
+}
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
